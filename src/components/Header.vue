cdec263e2f52a132bb3917c940b51c32e58acccd
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-01-26 15:55:44 +0100

    Added: header & title

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="header">Questo è un header</div>
+</template>
+
+
+<script>
+export default {
+}
+</script>
+
+
+<style>
+.header{
+    margin: 20px;
+}
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="title"> 
+      La mia prima app con Vue CLI!  
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.title{
+    font-size: 20px;
+    font-weight: bold;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+}
+</style>
